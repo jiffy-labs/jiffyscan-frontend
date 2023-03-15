cb53a2ed11c6383dab17534ad5c4e34ab4180bea
Don <don@crane.tech>
2023-03-15 02:53:48 -0700

    chore: fix margins/paddings for desktop

--- src/components/homepage/RecentMetrics.jsx
+++ src/components/homepage/RecentMetrics.jsx
@@ -1,17 +1,37 @@
 export default function RecentMetrics() {
     return (
-        <div className="flex flex-row space-x-2 overflow-auto">
-            <div className="w-[167.5px] lg:w-[200px] h-[156px] border-2 rounded shadow-lg">
-                Daily User Ops
+        <div className="overflow-auto w-full">
+            <div className="flex flex-row relative">
+                <h1 className="text-xl font-semibold pb-4 w-1/3">
+                    Recent Metrics
+                </h1>
+                <div className="absolute flex flex-row right-0 space-x-4 hidden lg:block">
+                    <button className="rounded-xl border-2 px-4">Goerli</button>
+                    <button className=" rounded-xl border-2 px-4">
+                        <span>Mumbai</span>
+                    </button>
+                    <button className=" rounded-xl border-2 px-4">
+                        <span>Optimism Goerli</span>
+                    </button>
+                    <button className=" rounded-xl border-2 px-4">
+                        <span>More</span>
+                    </button>
+                </div>
             </div>
-            <div className="w-[167.5px] lg:w-[200px] h-[156px] border-2 rounded shadow-lg">
-                Daily User Ops
-            </div>
-            <div className="w-[167.5px] lg:w-[200px] h-[156px] border-2 rounded shadow-lg">
-                Daily User Ops
-            </div>
-            <div className="w-[167.5px] lg:w-[200px] h-[156px] border-2 rounded shadow-lg">
-                Daily User Ops
+
+            <div className="flex flex-row space-x-2 overflow-auto">
+                <div className="w-[167.5px] lg:w-1/4 h-[156px] border-2 rounded shadow-lg">
+                    Daily User Ops
+                </div>
+                <div className="w-[167.5px] lg:w-1/4 h-[156px] border-2 rounded shadow-lg">
+                    Daily User Ops
+                </div>
+                <div className="w-[167.5px] lg:w-1/4 h-[156px] border-2 rounded shadow-lg">
+                    Daily User Ops
+                </div>
+                <div className="w-[167.5px] lg:w-1/4 h-[156px] border-2 rounded shadow-lg">
+                    Daily User Ops
+                </div>
             </div>
         </div>
     );
--- src/components/homepage/ViewAllUserOpsButton.jsx
+++ src/components/homepage/ViewAllUserOpsButton.jsx
@@ -3,7 +3,7 @@ import { IconChevronRight } from '@tabler/icons-react';
 
 export default function ViewAllUserOpsButton() {
     return (
-        <button className="w-[280px] lg:w-1/2 text-black font-semibold px-4 rounded h-12 uppercase border-2 border-[#B0BEC5]">
+        <button className="w-[280px] lg:w-1/2 text-black font-semibold pl-4 lg:px-4 rounded h-12 uppercase border-2 border-[#B0BEC5]">
             <div className="flex ">
                 View All User Operations
                 <IconChevronRight size={20} className="my-auto" />
