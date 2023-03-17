c2aa4090e2fe155aba744d5a43a9e4759d7ad96f
akashrahman-me <akashrahman.me@gmail.com>
2023-03-17 12:18:43 +0600

    ler

--- src/views/home/Table.tsx
+++ src/views/home/Table.tsx
@@ -340,8 +340,27 @@ function Table() {
           </tbody>
         </table>
       </div>
-      <div>
-        <div className="flex "></div>
+      <div className="flex justify-end items-center gap-6 text-sm">
+        <div className="flex items-center gap-3">
+          <p>Rows per page:</p>
+          <div className="flex items-center">
+            <span>20</span>
+            <img src="/images/menu-down.svg" alt="" />
+          </div>
+        </div>
+        <p>1–20 of 10000</p>
+        <div>
+          {[
+            "icon-container (18).svg",
+            "icon-container (19).svg",
+            "icon-container (20).svg",
+            "icon-container (21).svg",
+          ].map((icon, key) => (
+            <button key={key} type="button">
+              <img src={`/images/${icon}`} alt="" />
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
