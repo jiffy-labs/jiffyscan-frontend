18140371148f1ce49172d32e0743eb250dc7dd0a
Don <don@crane.tech>
2023-03-19 03:07:58 -0700

    feat: add network dropdown button

---
+++ src/components/common/chip/ChipDropdown.tsx
@@ -0,0 +1,112 @@
+import React, { ReactNode, Fragment, useState } from 'react';
+import sx from './chip.module.sass';
+import { Menu, Transition } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import { MORE_NETWORK_LIST } from '@/components/common/constants';
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ');
+}
+
+export interface ChipProps {
+    variant?: 'contained' | 'outlined';
+    color?: 'dark-700' | 'white' | 'info' | 'success' | 'dark-400';
+    className?: string;
+    isMoreSelected: boolean;
+    setIsMoreSelected: (isMoreSelected: boolean) => void;
+    onClickFcn: (network: string) => void;
+}
+
+function ChipDropdown(props: ChipProps) {
+    const {
+        variant = 'contained',
+        isMoreSelected,
+        className,
+        setIsMoreSelected,
+        onClickFcn,
+    } = props;
+
+    const color = isMoreSelected ? 'dark-700' : 'white';
+    const [selectedNetwork, setSelectedNetwork] = useState('ETH');
+
+    const [icon, setIcon] = useState<string | null>(null);
+
+    return (
+        <div className="text-sm">
+            <Menu as="div" className="relative inline-block text-left ">
+                <span>
+                    <Menu.Button
+                        className={`${sx.wrapper} ${sx[variant]} ${sx[color]} ${
+                            className || ''
+                        }`}
+                    >
+                        {icon && isMoreSelected && (
+                            <img
+                                src={icon}
+                                alt=""
+                                style={{
+                                    height: '12px',
+                                    width: '12px',
+                                }}
+                            />
+                        )}
+
+                        {isMoreSelected ? selectedNetwork : 'More'}
+                        <ChevronDownIcon
+                            className="-mr-1 h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                        />
+                    </Menu.Button>
+                </span>
+
+                <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <div className="py-1">
+                            {MORE_NETWORK_LIST.map(
+                                ({ name, key, iconPath }, index) => (
+                                    <Menu.Item>
+                                        {({ active }) => (
+                                            <a
+                                                onClick={() => {
+                                                    setSelectedNetwork(name);
+                                                    setIsMoreSelected(true);
+                                                    onClickFcn(key);
+                                                    setIcon(iconPath);
+                                                }}
+                                                className={classNames(
+                                                    active
+                                                        ? 'bg-gray-100 text-gray-900'
+                                                        : 'text-gray-700',
+                                                    'block px-4 py-2 text-sm'
+                                                )}
+                                            >
+                                                {name}
+                                            </a>
+                                            // 	<img
+                                            // 		src={startIcon}
+                                            // 		alt=""
+                                            // 		style={{ height: '12px', width: '12px' }}
+                                            // 	/>
+                                            // <span className="flex-1">{children}</span>
+                                            // {endIcon && <img src={endIcon} alt="" />}
+                                        )}
+                                    </Menu.Item>
+                                )
+                            )}
+                        </div>
+                    </Menu.Items>
+                </Transition>
+            </Menu>
+        </div>
+    );
+}
+
+export default ChipDropdown;
--- src/components/globals/recent_metrics/NetworkSelector.tsx
+++ src/components/globals/recent_metrics/NetworkSelector.tsx
@@ -1,22 +1,51 @@
-import React from 'react'
-import Chip from "@/components/common/chip/Chip";
+import React, { useState } from 'react';
+import Chip from '@/components/common/chip/Chip';
 import { NETWORK_LIST } from '@/components/common/constants';
+import ChipDropdown from '@/components/common/chip/ChipDropdown';
 
-function NetworkSelector({ selectedNetwork, handleNetworkChange }: { selectedNetwork: string, handleNetworkChange: (network: string) => void }) {
-    return (
-        <div className="flex flex-wrap items-center gap-1">
-            {NETWORK_LIST.map(({ name, key, iconPath, iconPathInverted }, index) => (
-                <Chip
-                    key={index}
-                    onClick={() => handleNetworkChange(key)}
-                    startIcon={selectedNetwork === key ? iconPath : iconPathInverted}
-                    color={`${selectedNetwork === key ? "dark-700" : "white"}`}
-                >
-                    {name}
-                </Chip>
-            ))}
-        </div>
-    )
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ');
 }
 
-export default NetworkSelector
+function NetworkSelector({
+    selectedNetwork,
+    handleNetworkChange,
+}: {
+    selectedNetwork: string;
+    handleNetworkChange: (network: string) => void;
+}) {
+    const [isMoreSelected, setIsMoreSelected] = useState(false);
+
+    return (
+        <div className="flex flex-wrap items-center gap-1">
+            {NETWORK_LIST.map(
+                ({ name, key, iconPath, iconPathInverted }, index) => (
+                    <Chip
+                        key={index}
+                        onClick={() => {
+                            handleNetworkChange(key);
+                            setIsMoreSelected(false);
+                        }}
+                        startIcon={
+                            selectedNetwork === key
+                                ? iconPath
+                                : iconPathInverted
+                        }
+                        color={`${
+                            selectedNetwork === key ? 'dark-700' : 'white'
+                        }`}
+                    >
+                        {name}
+                    </Chip>
+                )
+            )}
+            <ChipDropdown
+                onClickFcn={handleNetworkChange}
+                isMoreSelected={isMoreSelected}
+                setIsMoreSelected={setIsMoreSelected}
+            />
+        </div>
+    );
+}
+
+export default NetworkSelector;
