6cf34681200ba24bf1fbe1127f63159f90964fa4
lazycoder1 <gautamgsabhahit@gmail.com>
2023-03-23 12:43:09 +0530

    adding pagination ogic

--- src/views/latestUserOps/UserOperations.tsx
+++ src/views/latestUserOps/UserOperations.tsx
@@ -7,22 +7,45 @@ import Pagination from "@/components/common/table/Pagination";
 import table_data from "./table_data.json";
 import { NETWORK_LIST, NETWORK_ICON_MAP } from "@/components/common/constants";
 import { getCurrencySymbol, getTimePassed } from "@/components/common/utils";
-import { getLatestUserOps } from "@/components/common/apiCalls/jiffyApis";
+import { getDailyMetrics, getLatestUserOps } from "@/components/common/apiCalls/jiffyApis";
 import { useConfig } from "@/context/config";
 
-function UserOperations() {
-  const {selectedNetwork, setSelectedNetwork} = useConfig()
-  const [latestUserOpsTable, setLatestUserOpsTable] = useState<tableDataT>({
-    rows:[]
-  });
-  
-  
-  useEffect (() => {
-    refreshUserOpsTable(selectedNetwork);
-  }, [selectedNetwork]);
+const DEFAULT_PAGE_SIZE = 10;
 
-  const refreshUserOpsTable = async (network: string) => {
-    const userOps = await getLatestUserOps(network, 10, 0);
+function UserOperations() {
+  const { selectedNetwork, setSelectedNetwork } = useConfig()
+  const [latestUserOpsTable, setLatestUserOpsTable] = useState<tableDataT>({
+    rows: []
+  });
+  const [pageNo, setPageNo] = useState(0);
+  const [pageSize, _setPageSize] = useState(DEFAULT_PAGE_SIZE);
+  const [totalRows, setTotalRows] = useState(0);
+
+  const setPageSize = (size: number) => {
+    _setPageSize(size);
+    setPageNo(0);
+  }
+
+  useEffect(() => {
+    fetchTotalRows();
+  })
+
+  useEffect(() => {
+    refreshUserOpsTable(selectedNetwork, pageSize, pageNo);
+  }, [selectedNetwork, pageNo]);
+
+  const fetchTotalRows = async () => {
+    const oneDayMetrics = await getDailyMetrics(selectedNetwork, 1);
+    let presentDayMetrics;
+    if (oneDayMetrics.length > 0) {
+      presentDayMetrics = oneDayMetrics[0];
+    }
+    setTotalRows(parseInt(presentDayMetrics?.userOpsTotal || '0'));
+  }
+
+  const refreshUserOpsTable = async (network: string, pageSize: number, pageNo: number) => {
+    console.log('testing refresh', pageSize, pageNo)
+    const userOps = await getLatestUserOps(network, pageSize, pageNo);
     let newRows = [] as tableDataT["rows"];
     userOps.forEach(userOp => {
       newRows.push({
@@ -38,8 +61,8 @@ function UserOperations() {
       })
     });
     setLatestUserOpsTable({
-      ...latestUserOpsTable, 
-      rows: newRows.slice(0,10)
+      ...latestUserOpsTable,
+      rows: newRows.slice(0, 10)
     });
   }
 
@@ -51,14 +74,20 @@ function UserOperations() {
           <h1 className="font-bold text-3xl">User Operations</h1>
         </div>
       </section>
-      <RecentMetrics selectedNetwork={selectedNetwork} handleNetworkChange={setSelectedNetwork}/>
+      <RecentMetrics selectedNetwork={selectedNetwork} handleNetworkChange={setSelectedNetwork} />
       <section className="mb-10">
         <div className="container">
           <div>
-          <Table {...latestUserOpsTable} />
-            <Pagination 
-            setTable={setLatestUserOpsTable} 
-            table={latestUserOpsTable as tableDataT}
+            <Table {...latestUserOpsTable} />
+            <Pagination
+              table={latestUserOpsTable as tableDataT}
+              pageDetails={{
+                pageNo: pageNo,
+                setPageNo: setPageNo,
+                pageSize: pageSize,
+                setPageSize: setPageSize,
+                totalRows: totalRows
+              }}
             />
           </div>
         </div>
