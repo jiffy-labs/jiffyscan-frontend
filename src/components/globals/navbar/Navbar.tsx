084eef6ece10105d0f27a9bc4ccd3ee3785e9dd6
abdbodara <abd.bodara@gmail.com>
2023-03-24 17:33:11 +0530

    implemented tooltip for header and header text formatted in chart.

--- src/components/globals/navbar/Navbar.tsx
+++ src/components/globals/navbar/Navbar.tsx
@@ -13,10 +13,6 @@ import Copyright from '../footer/Copyright';
 import Donations from '../footer/Donations';
 import { ChevronRightIcon, HomeIcon } from '@heroicons/react/20/solid';
 
-const pages = [
-    // { name: 'Home', href: '/', current: false },
-    { name: 'LatestBundles', href: '#', current: true },
-];
 interface NavbarProps {
     searchbar?: boolean;
 }
@@ -62,20 +58,6 @@ function Navbar(props: NavbarProps) {
                                 </Link>
                             </div>
                         </li>
-                        {pages.map((page) => (
-                            <li key={page.name}>
-                                <div className="flex items-center">
-                                    <ChevronRightIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                                    <a
-                                        href={page.href}
-                                        className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
-                                        aria-current={page.current ? 'page' : undefined}
-                                    >
-                                        {page.name}
-                                    </a>
-                                </div>
-                            </li>
-                        ))}
                     </ol>
                 </nav>
                 <div className="min-h-screen flex justify-between flex-col gap-10 p-4 pb-6">
--- src/views/latestBundles/Bundles.tsx
+++ src/views/latestBundles/Bundles.tsx
@@ -15,10 +15,7 @@ import Link from 'next/link';
 
 const METRIC_DATA_POINT_SIZE = 14;
 const DEFAULT_PAGE_SIZE = 10;
-const pages = [
-    // { name: 'Home', href: '/', current: false },
-    { name: 'LatestBundles', href: '#', current: true },
-];
+
 function UserOperations() {
     const { selectedNetwork, setSelectedNetwork } = useConfig();
     const [latestBundlesTable, setLatestBundlesTable] = useState<tableDataT>(table_data as tableDataT);
@@ -83,32 +80,7 @@ function UserOperations() {
             <RecentMetrics selectedNetwork={selectedNetwork} handleNetworkChange={setSelectedNetwork} />
             <section className="mb-10">
                 <div className="container">
-                    <nav className="flex" aria-label="Breadcrumb">
-                        <ol role="list" className="flex items-center space-x-4">
-                            <li>
-                                <div>
-                                    <Link href="/" className="text-gray-400 hover:text-gray-500">
-                                        <HomeIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
-                                        <span className="sr-only">Home</span>
-                                    </Link>
-                                </div>
-                            </li>
-                            {pages.map((page) => (
-                                <li key={page.name}>
-                                    <div className="flex items-center">
-                                        <ChevronRightIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                                        <a
-                                            href={page.href}
-                                            className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
-                                            aria-current={page.current ? 'page' : undefined}
-                                        >
-                                            {page.name}
-                                        </a>
-                                    </div>
-                                </li>
-                            ))}
-                        </ol>
-                    </nav>
+                    
                     <div>
                         <Table
                             {...latestBundlesTable}
