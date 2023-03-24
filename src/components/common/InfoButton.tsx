a720ef593e68c27dabab9ce0214d0ee169def218
lazycoder1 <gautamgsabhahit@gmail.com>
2023-03-25 02:03:47 +0530

    commenting out tooltip

--- src/components/common/InfoButton.tsx
+++ src/components/common/InfoButton.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Tooltip } from 'react-tooltip';
+// import { Tooltip } from 'react-tooltip';
 import 'react-tooltip/dist/react-tooltip.css';
 function InfoButton({ data }: { data?: string }) {
     return (
         <>
-            <Tooltip id="my-tooltip" style={{ borderRadius: '0.35rem' }} />
+            {/* <Tooltip id="my-tooltip" style={{ borderRadius: '0.35rem' }} /> */}
             <div className="flex rounded-full" data-tooltip-id="my-tooltip" data-tooltip-content={data}>
                 <button type="button">
                     <img src="/images/icon-container (3).svg" alt="" />
