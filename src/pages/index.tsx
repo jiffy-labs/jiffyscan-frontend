82a6a8403cb734c42f57725819ff576d3cdc396d
abdbodara <abd.bodara@gmail.com>
2023-03-30 11:37:54 +0530

    implemented icon for the more network filter and append network parameter for the index page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,42 @@
-import Layout from '@/components/globals/Layout';
+import { useRouter } from 'next/router';
+import { useConfig } from '@/context/config';
 import Home from '@/views/home/Home';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
+import Layout from '@/components/globals/Layout';
 
-function index() {
+function Index() {
+    const { selectedNetwork } = useConfig();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (selectedNetwork) {
+            // Get the current query parameters
+            const { query } = router;
+
+            // Append the new parameter
+            const newQuery = {
+                ...query,
+                selectedNetwork,
+            };
+
+            // Construct the new URL with the updated query parameters
+            const href = {
+                pathname: '/',
+                query: newQuery,
+            };
+
+            // Navigate to the new URL without causing a full page refresh
+            router.push(href, undefined, { shallow: true });
+        }
+    }, [selectedNetwork]);
+
     return (
-        <div>
+        <Layout>
             <Home />
-        </div>
+        </Layout>
     );
 }
 
-export default index;
+export default Index;
 
-index.getLayout = (page: ReactElement) => <Layout>{page}</Layout>;
+Index.getLayout = (page: ReactElement) => <Layout>{page}</Layout>;
