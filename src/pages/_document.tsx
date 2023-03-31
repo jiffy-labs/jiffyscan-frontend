c40a2719c0b862e2e923df058311c7b671c01127
lazycoder1 <gautamgsabhahit@gmail.com>
2023-03-31 19:07:14 +0530

    fixing favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import favicon from '@/public/favicon.ico';
 
 export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+                <link rel="icon" href="./favicon.ico" />
+                <title>Jiffy Scan</title>
             </Head>
             <body>
                 <Main />
