6b432168f081d1ffd41a3bceba2298b277eeccdf
lazycoder1 <gautamgsabhahit@gmail.com>
2023-03-31 20:39:49 +0530

    testing out metatags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import favicon from '@/public/favicon.ico';
 
 export default function Document() {
     return (
         <Html lang="en">
             <Head>
                 <link rel="icon" href="./favicon.ico" />
-                <title>Jiffy Scan</title>
+                <meta name="description" content="User Operation explorer for Account Abstraction (ERP-4337)" />
+                <meta name="keywords" content="4337, Account Abstraction, Explorer, BlockChain, Paymaster, Bundler, Entrypoint"></meta>
+                <meta name="author" content="Jiffy Labs"></meta>
+                <meta property="og:title" content="Jiffy Scan - User Op Explorer" />
+                <meta property="og:type" content="blockchain explorer" />
+                <meta property="og:url" content="https://www.jiffyscan.xyz/" />
+                <meta property="og:image" content="./favicon.ico" />
+                <meta name="twitter:card" content="summary_large_image"></meta>
+                <title>Jiffy Scan - User Op Explorer</title>
             </Head>
             <body>
                 <Main />
