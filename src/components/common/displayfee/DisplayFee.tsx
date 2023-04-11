99a97e104e6e1857f7c4574943ce79c1b40c1557
lazycoder1 <gautamgsabhahit@gmail.com>
2023-04-11 23:37:36 +0530

    fixing fee edge case

--- src/components/common/displayfee/DisplayFee.tsx
+++ src/components/common/displayfee/DisplayFee.tsx
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Chip, { ChipProps } from '../chip/Chip';
 import CopyButton from '../copy_button/CopyButton';
 import { getFee } from '../utils';
 
 export default function DisplayFee({ item, network }: any) {
     const [fee, setFee] = useState(getFee(item, network));
+
+    useEffect(() => {
+        setFee(getFee(item, network));
+    }, [item, network])
+
     return (
         <div>
             <div className="flex items-center text-rgiht gap-2">
