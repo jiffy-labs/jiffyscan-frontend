678cd6ceebe176a7e3f773e950a6d1f0e60ae5a2
lazycoder1 <gautamgsabhahit@gmail.com>
2023-04-27 20:01:19 +0530

    adding bundlers and paymasters talbe in home page

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -5,11 +5,13 @@ import Navbar from '@/components/globals/navbar/Navbar';
 import RecentMetrics from '@/components/globals/recent_metrics/RecentMetrics';
 import React, { useState, useEffect } from 'react';
 import BundlesTable from './bundles_table.json';
+import BundlersTable from './bundlers_table.json';
+import PaymastersTable from './paymasters_table.json';
 import OperationsTable from './operations_table.json';
 import Searchblock from '../../components/globals/searchblock/Searchblock';
 import { NETWORK_ICON_MAP } from '@/components/common/constants';
-import { getLatestBundles, getLatestUserOps } from '@/components/common/apiCalls/jiffyApis';
-import { getTimePassed } from '@/components/common/utils';
+import { getLatestBundles, getLatestUserOps, getTopBundlers, getTopPaymasters } from '@/components/common/apiCalls/jiffyApis';
+import { getFee, getTimePassed } from '@/components/common/utils';
 import { useConfig } from '../../context/config';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -18,17 +20,17 @@ function Home() {
     const { selectedNetwork, setSelectedNetwork } = useConfig();
     const [bundlesTable, setBundlesTable] = useState<tableDataT>(BundlesTable as tableDataT);
     const [operationsTable, setOperationsTable] = useState<tableDataT>(OperationsTable as tableDataT);
+    const [bundlersTable, setBundlersTable] = useState<tableDataT>(BundlersTable as tableDataT);
+    const [paymastersTable, setPaymastersTable] = useState<tableDataT>(PaymastersTable as tableDataT);
     const [userOpTableLoading, setUserOpTableLoading] = useState(true);
     const [bundleTableLoading, setBundleTableLoading] = useState(true);
+    const [bundlerTableLoading, setBundlerTableLoading] = useState(true);
+    const [paymasterTableLoading, setPaymasterTableLoading] = useState(true);
     useEffect(() => {
         refreshBundlesTable(selectedNetwork);
         refreshUserOpsTable(selectedNetwork);
-        setTimeout(() => {
-            setBundleTableLoading(false);
-        }, 2000);
-        setTimeout(() => {
-            setUserOpTableLoading(false);
-        }, 2000);
+        refreshBundlersTable(selectedNetwork);
+        refreshPaymastersTable(selectedNetwork);
     }, [selectedNetwork]);
 
     const refreshBundlesTable = async (network: string) => {
@@ -48,10 +50,44 @@ function Home() {
             });
         });
         setBundlesTable({ ...bundlesTable, rows: newRows.slice(0, 5) });
-        // setTimeout(() => {
-        //     setBundleTableLoading(false);
-        // }, 3000);
+        setBundleTableLoading(false);
     };
+
+    const refreshPaymastersTable = async (network: string) => {
+        const paymasters = await getTopPaymasters(network, 5, 0, toast);
+        let newRows: tableDataT['rows'] = [];
+        paymasters.forEach((paymaster) => {
+            newRows.push({
+                token: {
+                    text: paymaster.address,
+                    icon: NETWORK_ICON_MAP[network],
+                    type: 'paymaster',
+                },
+                userOps: `${paymaster.userOpsLength} ops`,
+            });
+        });
+        setPaymastersTable({ ...paymastersTable, rows: newRows.slice(0, 10) });
+        setPaymasterTableLoading(false);
+    };
+
+    const refreshBundlersTable = async (network: string) => {
+        const bundlers = await getTopBundlers(network, 5, 0, toast);
+        let newRows: tableDataT['rows'] = [];
+        bundlers.forEach((bundler) => {
+            newRows.push({
+                token: {
+                    text: bundler.address,
+                    icon: NETWORK_ICON_MAP[network],
+                    type: 'bundler',
+                },
+                userOps: `${bundler.bundleLength} bundles`,
+                fee: getFee(parseInt(bundler.actualGasCostSum), network),
+            });
+        });
+        console.log(newRows);
+        setBundlersTable({ ...bundlersTable, rows: newRows });
+        setBundlerTableLoading(false);
+    }
 
     const refreshUserOpsTable = async (network: string) => {
         setUserOpTableLoading(true);
@@ -71,9 +107,7 @@ function Home() {
             });
         });
         setOperationsTable({ ...operationsTable, rows: newRows.slice(0, 5) });
-        // setTimeout(() => {
-        //     setUserOpTableLoading(false);
-        // }, 3000);
+        setUserOpTableLoading(false);
     };
 
     return (
@@ -122,6 +156,39 @@ function Home() {
                         />
                         <div className="mt-4">
                             <Button href="/recentUserOps">View all User operations</Button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="mb-12">
+                <div className="container grid-cols-1 md:grid-cols-2 grid gap-10">
+                    <div>
+                        <Table
+                            {...(bundlersTable as tableDataT)}
+                            loading={bundlerTableLoading}
+                            caption={{
+                                children: 'Top Bundlers',
+                                icon: '/images/swap-vertical-bold (1).svg',
+                                text: 'Top Bundlers by selected chain',
+                            }}
+                        />
+
+                        <div className="mt-4">
+                            <Button href="/bundlers">View all Bundlers</Button>
+                        </div>
+                    </div>
+                    <div>
+                        <Table
+                            {...(paymastersTable as tableDataT)}
+                            loading={paymasterTableLoading}
+                            caption={{
+                                children: 'Top Paymasters',
+                                icon: '/images/swap-vertical-bold (1).svg',
+                                text: 'Top Paymaster by selected chain',
+                            }}
+                        />
+                        <div className="mt-4">
+                            <Button href="/payamsters">View all Paymasters</Button>
                         </div>
                     </div>
                 </div>
