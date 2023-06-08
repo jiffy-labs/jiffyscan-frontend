19c301a4893fdd6126ec30e2ce7b750bd42b6800
lazycoder1 <gautamgsabhahit@gmail.com>
2023-06-08 17:27:32 +0530

    removing tool trip from headers

--- src/components/common/table/Caption.tsx
+++ src/components/common/table/Caption.tsx
@@ -13,7 +13,7 @@ function Caption(props: CaptionProps) {
         <div className="flex items-center gap-2 py-2 mb-2">
             <img src={icon} alt="" />
             <span className="font-bold text-lg leading-[1.2]">{children}</span>
-            <InfoButton data={text} />
+            {text && <InfoButton data={text} />}
         </div>
     );
 }
