0bfc0993277883b2ad8028823c4eba67d409ba54
Dheeraj singh <47826524+dheerajsingh0@users.noreply.github.com>
2023-08-08 18:01:03 +0000

    96-dheerajsingh0-Buttons-bottom-padding-too-small

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -12,7 +12,7 @@ function Button(props: ButtonProps) {
     return (
         <Link
             href={href}
-            className="font-medium border border-dark-200 text-md gap-2 inline-flex items-center tracking-[1.25px] pt-[9px] pb-[5px] px-4 uppercase hover:no-underline rounded bg-white"
+            className="font-medium border border-dark-200 text-md gap-2 inline-flex items-center tracking-[1.25px] pt-[9px] pb-[9px] px-4 uppercase hover:no-underline rounded bg-white"
         >
             <span>{children}</span>
             <img className="-translate-y-[1px]" src="/images/chevron-right.svg" alt="" />
