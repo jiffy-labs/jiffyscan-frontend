644a29e55c78ce69d678bcabbddbcb24145e6eea
lazycoder1 <gautamgsabhahit@gmail.com>
2023-09-29 21:54:46 +0530

    updating popup origin

--- src/pages/callbackPopup.tsx
+++ src/pages/callbackPopup.tsx
@@ -3,12 +3,13 @@ import auth0 from 'auth0-js';
 
 
 const CallbackPopup = () => {
+  let callbackUrl = window.location.protocol + "//" + window.location.host
   useEffect(() => {
     let auth = new auth0.WebAuth({
       domain: 'https://dev-xbhwhdj3wrltd7bv.us.auth0.com',
       clientID: 'VRNrC8mxH3DP7v16nT41siK7zIqS8Whl',
-      redirectUri: 'http://localhost:3000',
-      popupOrigin: 'http://localhost:3000'
+      redirectUri: callbackUrl,
+      popupOrigin: callbackUrl
     });
 
     auth.popup.callback({hash: window.location.hash});
