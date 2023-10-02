4d956d5867c9c8d17d98ee2e0199cbe9e75c1185
lazycoder1 <gautamgsabhahit@gmail.com>
2023-10-02 20:46:49 +0530

    adding heap

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import SEO from '@/components/common/SEO';
 import { Html, Head, Main, NextScript } from 'next/document';
 import Link from 'next/link';
+import Script from 'next/script';
 
 export default function Document() {
     return (
@@ -11,6 +12,16 @@ export default function Document() {
             <body>
                 <Main />
                 <NextScript />
+                <Script
+                    id="heap-analytics"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+      window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+      heap.load("1613731307");
+      `,
+                    }}
+                />
             </body>
         </Html>
     );
