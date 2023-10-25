c7e12f19d2eba13e07688eea18cb24a26d10acfd
lazycoder1 <gautamgsabhahit@gmail.com>
2023-10-25 11:43:25 +0530

    bringing twitter login back

--- src/context/userSession.tsx
+++ src/context/userSession.tsx
@@ -34,39 +34,39 @@ export default function UserSessionStore({ children }: any) {
 
     useEffect(() => {
         if (localStorage) {
-            setBypass(localStorage.getItem('bypass') == 'true');
+            // setBypass(localStorage.getItem('bypass') == 'true');
             setInitiate(true);
         }
     }, [])
 
-    useEffect(() => {
-        if (localStorage && initiate) {
-            // console.log('bypass', bypass)
-            localStorage.setItem('bypass', bypass.toString());
-        }
+    // useEffect(() => {
+    //     if (localStorage && initiate) {
+    //         // console.log('bypass', bypass)
+    //         localStorage.setItem('bypass', bypass.toString());
+    //     }
 
-    }, [bypass, initiate])
+    // }, [bypass, initiate])
 
 
     const isLoggedIn = () => {
-        if (bypass) return true;
+        // if (bypass) return true;
         const expiryTime = session?.user?.expires_at ? session.user.expires_at : session?.user?.exp;
         return expiryTime ? expiryTime > (Date.now() / 1000) : false;
     }
 
     const signOut = () => {
-        if (bypass) {
-            setBypass(false);
-            return;
-        }
+        // if (bypass) {
+        //     setBypass(false);
+        //     return;
+        // }
         NextAuth.signOut();
     }
     
     const signIn = (social: Social) => {
-        if (social == Social.TWITTER) {
-            setBypass(true);
-            return;
-        }
+        // if (social == Social.TWITTER) {
+        //     setBypass(true);
+        //     return;
+        // }
         NextAuth.signIn(social);
     };
 
