4e38ad520c02b2fd032cad4bcaaf9416cd68f221
lazycoder1 <gautamgsabhahit@gmail.com>
2024-01-12 14:38:30 +0530

    search bar fixing

--- src/components/global/searchblock/Searchblock.tsx
+++ src/components/global/searchblock/Searchblock.tsx
@@ -23,6 +23,7 @@ function Searchblock({ isNavbar }: { isNavbar: boolean }) {
     const { push } = useRouter();
     const [searching, setSearching] = useState(false);
     const [term, setTerm] = useState('');
+    const [animateState, setAnimateState] = useState(false);
     const searchRef: any = useRef(null);
 
     const handleChange = (e: any) => setTerm(e.target.value.trim());
@@ -35,8 +36,15 @@ function Searchblock({ isNavbar }: { isNavbar: boolean }) {
 
     const keyDownHandler = (event: KeyboardEvent) => {
         if (event.ctrlKey && event.key === 'k') {
+            event.preventDefault();
             console.log('You just pressed Control and K!');
             if (searchRef.current) searchRef.current.focus();
+            // animate for 1 second
+            setAnimateState(true);
+
+            setTimeout(() => {
+                setAnimateState(false);
+            }, 500);
         }
     };
 
@@ -73,12 +81,19 @@ function Searchblock({ isNavbar }: { isNavbar: boolean }) {
 
     if (isNavbar) {
         return (
-            <div className="shadow-300 bg-white rounded border-dark-200 flex-grow max-w-[400px]">
+            <div
+                className={`shadow-300 bg-white rounded border-dark-200 flex-grow max-w-[400px] ${
+                    animateState ? `focus-within:translate-y-2 focus-within:-translate-x-2   focus-within:scale-125` : ''
+                } duration-150`}
+                //     className={`shadow-300 bg-white rounded border-dark-200 flex-grow max-w-[400px]
+                //     focus-within:translate-y-2 focus-within:-translate-x-2   focus-within:scale-125
+                // duration-150`}
+            >
                 <label className="flex justify-center">
                     <span onClick={handleSubmit} className="p-2.5 border-r border-dark-200" role="button">
                         <img src="/images/search.svg" alt="" />
                     </span>
-                    <div className="flex items-center gap-2.5 pr-3 flex-grow">
+                    <div className="flex items-center gap-2.5 pr-3 flex-grow focus-within:shadow-xl">
                         <input
                             type="text"
                             className="flex-grow px-3 py-2 text-base placeholder:text-dark-500 text-dark-600"
@@ -99,13 +114,17 @@ function Searchblock({ isNavbar }: { isNavbar: boolean }) {
         );
     } else {
         return (
-            <div className="shadow-300 bg-white rounded border-dark-200 flex-grow max-w-[812px]">
+            <div
+                className={`shadow-300 bg-white rounded border-dark-200 flex-grow max-w-[812px] 
+                     duration-120
+                    ${animateState ? `focus-within:translate-y-2 focus-within:-translate-x-2   focus-within:scale-125` : ''} duration-150`}
+            >
                 <label className="flex justify-center">
                     {/* <Options networkValue={networkValue} setNetworkValue={setNetworkValue} /> */}
-                    <div className="flex items-center gap-2.5 pr-4 flex-grow">
+                    <div className="flex items-center gap-2.5 pr-4 flex-grow focus-within:shadow-xl ">
                         <input
                             type="text"
-                            className="text-base placeholder:text-dark-500 text-dark-600 px-4 py-2 flex-grow truncate min-w-0 max-w-none w-[0px]"
+                            className="text-base placeholder:text-dark-500  text-dark-600 px-4 py-2 flex-grow truncate min-w-0 max-w-none w-[0px]"
                             placeholder="Search by block number, address, hash, or userOp hash..."
                             value={term}
                             onChange={handleChange}
@@ -118,7 +137,7 @@ function Searchblock({ isNavbar }: { isNavbar: boolean }) {
                     </div>
                     <div
                         role="button"
-                        className="flex items-center gap-2 py-3.5 px-5 bg-dark-600 rounded-r text-white font-medium text-md tracking-[1.25px] uppercase"
+                        className="flex items-center gap-2 py-3.5 px-5  bg-dark-600 rounded-r text-white font-medium text-md tracking-[1.25px] uppercase"
                         onClick={handleSubmit}
                     >
                         <img src="/images/icon-container (25).svg" alt="" />
