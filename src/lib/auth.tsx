775f73f6c43014a286936f148577a4593532eead
lazycoder1 <gautamgsabhahit@gmail.com>
2024-02-07 23:54:27 +0530

    removing authorize

--- src/lib/auth.tsx
+++ src/lib/auth.tsx
@@ -1,10 +1,9 @@
-import {Provider} from "next-auth/providers";
-import CredentialsProvider from 'next-auth/providers/credentials'
-import authorize from "@/lib/authorize";
-import jwt from "@/lib/jwtAuthCallBack";
-import manageSession from "@/lib/sessionCallBack";
-import TwitterProvider from "next-auth/providers/twitter";
-import GithubProvider from "next-auth/providers/github";
+import { Provider } from 'next-auth/providers';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import jwt from '@/lib/jwtAuthCallBack';
+import manageSession from '@/lib/sessionCallBack';
+import TwitterProvider from 'next-auth/providers/twitter';
+import GithubProvider from 'next-auth/providers/github';
 
 const NEXTAUTH_URL = process.env.NEXTAUTH_URL;
 const COGNITO_REGION = process.env.NEXT_PUBLIC_COGNITO_REGION;
@@ -17,22 +16,20 @@ const TWITTER_CLIENT_SECRET = process.env.TWITTER_CLIENT_SECRET;
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
-console.log("Process env:", process.env)
-
+console.log('Process env:', process.env);
 
 export const authOptions = {
-
     providers: [
         // ...["Google"].map((provider) => getProvider(provider)),
         TwitterProvider({
-            clientId: TWITTER_CLIENT_ID ? TWITTER_CLIENT_ID : "",
-            clientSecret: TWITTER_CLIENT_SECRET ? TWITTER_CLIENT_SECRET : "",
-            version: "2.0",
-          }),
+            clientId: TWITTER_CLIENT_ID ? TWITTER_CLIENT_ID : '',
+            clientSecret: TWITTER_CLIENT_SECRET ? TWITTER_CLIENT_SECRET : '',
+            version: '2.0',
+        }),
         GithubProvider({
-            clientId: GITHUB_CLIENT_ID ? GITHUB_CLIENT_ID : "",
-            clientSecret: GITHUB_CLIENT_SECRET ? GITHUB_CLIENT_SECRET : "",
-        })
+            clientId: GITHUB_CLIENT_ID ? GITHUB_CLIENT_ID : '',
+            clientSecret: GITHUB_CLIENT_SECRET ? GITHUB_CLIENT_SECRET : '',
+        }),
         // CredentialsProvider({
         //     type: "credentials",
         //     id: 'Credentials_signIn',
@@ -56,14 +53,14 @@ export const authOptions = {
             return true;
         },
         async redirect(url: any) {
-            console.log("Url======>", url)
+            console.log('Url======>', url);
             // Return the url to redirect to after successful sign in.
             // Allows relative callback URLs
 
-            if (url.url.startsWith("/")) return `${url.baseUrl}${url.url}`
+            if (url.url.startsWith('/')) return `${url.baseUrl}${url.url}`;
             // Allows callback URLs on the same origin
-            else if (new URL(url.url)?.origin === url.baseUrl) return url.url
-            return url.baseUrl
+            else if (new URL(url.url)?.origin === url.baseUrl) return url.url;
+            return url.baseUrl;
         },
         jwt: jwt,
         // async jwt({token, account, profile, user}: any) {
@@ -73,8 +70,6 @@ export const authOptions = {
         // return session; // Make sure to return the modified session
     },
     pages: {
-        signIn: '/'
-      }
+        signIn: '/',
+    },
 };
-
-
