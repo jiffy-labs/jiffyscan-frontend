ddffa078916b7171ddb6fb434df189408a7e55c9
Ojas13-git <79032848+Ojas13-git@users.noreply.github.com>
2024-07-02 23:39:11 +0530

    feature/visit-dashboard-btn

--- src/components/global/navbar/pages/Pages.tsx
+++ src/components/global/navbar/pages/Pages.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import pages from './pages.json';
 import Menu from './Menu';
 
@@ -16,6 +16,19 @@ export default function Pages() {
                     return <Menu key={id} name={name} dropdown={dropdown} id={id.toString()} />;
                 }
             })}
+
+            
+                <Link legacyBehavior href="https://dashboard.jiffyscan.xyz/" passHref>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="lg:hidden bg-[#275EC9] text-white no-underline hover:no-underline rounded-[27px] h-[40px] px-[24px] py-[8px] flex items-center justify-center gap-2 font-roboto text-base font-normal leading-[20px]"
+                    >
+                        Visit Dashboard
+                    </a>
+                </Link>
+            
         </div>
     );
 }
+
