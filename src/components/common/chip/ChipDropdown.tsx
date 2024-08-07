ca9d564d46a2f21377cb3709fd4e7da405cab799
Manan <mvpmvp8989@gmail.com>
2024-08-07 19:43:22 +0530

    Fixed Overlapping Dropdown Menus

--- src/components/common/chip/ChipDropdown.tsx
+++ src/components/common/chip/ChipDropdown.tsx
@@ -46,7 +46,7 @@ function ChipDropdown(props: ChipProps) {
     }, [searchTerm, dropdownNetworkList]);
     return (
         <div className="text-sm">
-            <Menu as="div" className="relative inline-block text-left ">
+            <Menu as="div" className="inline-block text-left ">
                 <span>
                     <Menu.Button className={`${sx.wrapper} ${sx[variant]} ${sx[color]} ${className || ''} ${disabled ? sx.disabled : ''}`}>
                         {(icon && isMoreSelected) && (
@@ -74,7 +74,7 @@ function ChipDropdown(props: ChipProps) {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                 >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right bg-white rounded-md shadow-lg w-52 ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute right-20 md:right-0 z-10 mt-2 origin-top-right bg-white rounded-md shadow-lg w-52 ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <div className='max-h-56 overflow-y-auto rounded-md p-2'>
                         <div className="flex flex-col py-1">
                              <input
