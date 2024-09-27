3644cfa72347ed3f641984e1157a515970285183
lazycoder1 <gautamgsabhahit@gmail.com>
2024-09-27 22:57:40 +0530

    build fix

--- src/components/global/footer/Footer.tsx
+++ src/components/global/footer/Footer.tsx
@@ -69,6 +69,7 @@ function Footer() {
                         <Donations />
                     </div>
                     <Script
+                        id="dealfront"
                         dangerouslySetInnerHTML={{
                             __html: `(function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('lYNOR8xyObe7WQJZ');`,
                         }}
