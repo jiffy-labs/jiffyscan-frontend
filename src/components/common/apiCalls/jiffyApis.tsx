8bf2f18e0d1a9e56ace7746518828a432a31396b
Ojas13-git <79032848+Ojas13-git@users.noreply.github.com>
2024-10-05 23:32:14 +0530

    tracer section added for base

--- src/components/common/apiCalls/jiffyApis.tsx
+++ src/components/common/apiCalls/jiffyApis.tsx
@@ -382,6 +382,29 @@ export const getUserOpMetadata = async (userOpHash: string, network: string, toa
     return data;
 };
 
+export const getUsserOpTrace = async (userOpHash: string, network: string, toast: any): Promise<metadata> => {
+    // if (!performApiCall(network)) return {} as metadata;
+    // if (network != 'mainnet') return {} as metadata;
+    let response;
+    try {
+        response = await fetch(`https://api-dev.jiffyscan.xyz/v0/getUserOpTraces?userOpHash=${userOpHash}&network=${network}`, {
+            method : 'GET',
+            headers: { 'x-api-key': 'gFQghtJC6F734nPaUYK8M3ggf9TOpojkbNTH9gR5' },
+        });
+    } catch (e) {
+        //alert(e.message)
+        showToast(toast, 'Error fetching metadata');
+        return {} as metadata;
+    }
+    if (response.status != 200) {
+        showToast(toast, 'Error fetching metadata');
+        return {} as metadata;
+    }
+    const data = await response.json();
+    
+    return data;
+};
+
 export const populateERC20TransfersWithTokenInfo = async (metaData: metadata): Promise<metadata> => {
     let populatedMetaData = metaData;
     await Promise.all(
