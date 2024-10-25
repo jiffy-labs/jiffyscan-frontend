35ba155afe14f7e6c5a00c2bb2b70af774abae5f
Ojas13-git <79032848+Ojas13-git@users.noreply.github.com>
2024-10-25 15:02:46 +0530

    account + factory page v2

--- src/views/factory/Factory.tsx
+++ src/views/factory/Factory.tsx
@@ -2,7 +2,7 @@ import Footer from '@/components/global/footer/Footer';
 import Navbar from '@/components/global/navbar/Navbar';
 import React, { useEffect, useState } from 'react';
 import { getBundleDetails, UserOp, AccountDetail, Bundle, getFactoryDetails, FactoryDetails } from '@/components/common/apiCalls/jiffyApis';
-import { Breadcrumbs, Link } from '@mui/material';
+import { Breadcrumbs, IconButton, Link, Tooltip } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useRouter } from 'next/router';
 import { getFee, getTimePassed, shortenString } from '@/components/common/utils';
@@ -17,6 +17,8 @@ import HeaderSection from './HeaderSection';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useConfig } from '@/context/config';
+import { SlHome } from 'react-icons/sl';
+import { MdContentCopy } from 'react-icons/md';
 
 // import Skeleton from '@/components/Skeleton';
 export const BUTTON_LIST = [
@@ -79,7 +81,13 @@ function Factory(props: any) {
     const [pageNo, setPageNo] = useState(0);
     const [pageSize, _setPageSize] = useState(DEFAULT_PAGE_SIZE);
     const [captionText, setCaptionText] = useState('Loading User Ops');
+    const [copyTooltip, setCopyTooltip] = useState('Copy'); // Tooltip state for copy action
 
+    const handleCopy = () => {
+        navigator.clipboard.writeText(hash); // Copy the hash to clipboard
+        setCopyTooltip('Copied!'); // Change tooltip to indicate success
+        setTimeout(() => setCopyTooltip('Copy'), 1500); // Reset tooltip after 1.5s
+    };
     // handling table page change. Everytime the pageNo change, or pageSize change this function will fetch new data and update it.
     const updateRowsData = async (network: string, pageNo: number, pageSize: number) => {
         setTableLoading(true);
@@ -135,9 +143,9 @@ function Factory(props: any) {
     }, [hash, network]);
     let skeletonCards = Array(5).fill(0);
     return (
-        <div className="">
+        <div className="dark:bg-[#191A23]">
             <Navbar searchbar />
-            <section className="px-3 py-10">
+            <section className="px-3 container mx-auto my-6 py-6 bg-white dark:bg-[#1F202B] shadow-lg rounded-xl border border-[#D7DAE0] dark:border-[#3B3C40]">
                 <div className="container px-0">
                     <div className="flex flex-row">
                         <Link href="/" className="text-gray-500">
@@ -145,24 +153,30 @@ function Factory(props: any) {
                                 style={{ height: '15px', width: '15px', marginRight: '20px', marginLeft: '10px', marginBottom: '3px' }}
                             />
                         </Link>
-                        <Breadcrumbs aria-label="breadcrumb">
+                        <Breadcrumbs aria-label="breadcrumb"  className="font-gsans text-[#646D8F] text-md sm:text-base">
                             <Link underline="hover" color="inherit" href={`/?network=${network ? network : ''}`}>
-                                Home
+                            <SlHome />
                             </Link>
                             <Link
                                 underline="hover"
                                 color="text.primary"
                                 href={`/factory/${hash}?network=${network ? network : ''}`}
                                 aria-current="page"
+                                className="text-[#195BDF]"
                             >
                                 {shortenString(hash as string)}
+                                <Tooltip title={copyTooltip}>
+                                    <IconButton onClick={handleCopy} size="small" style={{ marginLeft: '8px' }}>
+                                        <MdContentCopy className="w-4 h-4 dark:fill-[#ADB0BC]" />
+                                    </IconButton>
+                                </Tooltip>
                             </Link>
                         </Breadcrumbs>
                     </div>
-                    <h1 className="text-3xl font-bold">Factory</h1>
+                    <h1 className="text-3xl font-bold p-4 font-gsans dark:text-white">Factory</h1>
                 </div>
-            </section>
-            <HeaderSection item={factoryInfo} network={network} addressMapping={addressMapping}/>
+            
+            {/* <HeaderSection item={factoryInfo} network={network} addressMapping={addressMapping}/> */}
             {/* <TransactionDetails item={factoryInfo} network={network} /> */}
             <div className="container px-0">
                 <Table
@@ -185,6 +199,7 @@ function Factory(props: any) {
                     }}
                 />
             </div>
+            </section>
             <ToastContainer />
             <Footer />
         </div>
